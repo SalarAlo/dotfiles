b23221dea94786614f059b3b1f0d2bb3b1a29907
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-09-04 14:23:52 +0500

    migrate: add startup commands from hyprland.conf

--- hyprland.lua
+++ hyprland.lua
@@ -3,3 +3,10 @@ hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("GTK_THEME", "Adwaita:dark")
 
 
+hl.on("hyprland.start", function()
+  hl.exec_cmd("waybar -c ~/.config/waybar/config.json -s ~/.config/waybar/style.css &")
+  hl.exec_cmd("mako")
+  hl.exec_cmd("$HOME/.config/hypr/set-wallpaper.sh")
+end)
+
+
